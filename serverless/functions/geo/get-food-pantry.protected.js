ba7b696c24ec677a6c5a7782318deb9875bf9fbb
Maddy Muscari <mmuscari@twilio.com>
2019-08-16 08:40:09 -0700

    filters by city restrictions

--- serverless/functions/geo/get-food-pantry.protected.js
+++ serverless/functions/geo/get-food-pantry.protected.js
@@ -55,6 +55,31 @@ function findPantries(db, lat, lon) {
     });
 }
 
+function isValidPantryForCity(db, pantry, city) {
+    let cityPromise = new Promise((complete, reject) => {
+	let query = "select id from cities where name=? limit 1"
+	db.all(query, [city], function(err, rows) {
+	    if(err) {
+		reject(err);
+	    } else {
+		complete(rows[0]);
+	    }
+	});
+    });
+    return cityPromise.then((city_id) => {
+	let query = "select (count(1) == 0) or count(case city_id when ? then 1 else 0 end)>0 from city_restrictions  where pantry_id=?"
+	return new Promise((complete, reject) =>
+			   {
+			       db.all(query, [city, pantry.id], function(err, rows) {
+				   if(err) {
+				       reject(err);
+				   } else {
+				       complete(rows[0]);
+				   }
+			       })});
+    });    
+}
+
 function respondWithPantry(formatted_address, lat, lng, pantry_name, pantry_address, pantry_city, pantry_state, callback) {
     let responseObject = {
 	"actions": [
@@ -77,7 +102,7 @@ function respondWithPantry(formatted_address, lat, lng, pantry_name, pantry_addr
 }
 
 exports.handler = function(context, event, callback) {
-
+    
     let contextChain = {};
     
     let memory = JSON.parse(event.Memory);
@@ -98,34 +123,27 @@ exports.handler = function(context, event, callback) {
 
     var db = new spatialite.Database('/tmp/pantries.sqlite');
 
-    let address_res
-    let formatted_address
-    let address_geo
-    let address_lat
-    let address_lng
-    
-    
-    db.spatialite(function(err) {
-	let matchedCityPromise = matchCity(db, city);
-	let addressLatLonPromise = matchedCityPromise.then( (matchedCity) => getAddressLatLon(context, address, matchedCity, "CA"));
-	let dbResult = addressLatLonPromise.then((addressLatLon) => {
-	    console.log(addressLatLon);
+    db.spatialite(async function(err) {
+	let matchedCity = await matchCity(db, city);
+	let addressLatLon = await getAddressLatLon(context, address, matchedCity, "CA");
+	console.log(addressLatLon);
+	
+	let address_res = addressLatLon.candidates[0]
+	let formatted_address = address.formatted_address
+	
+	let address_geo = address_res.geometry
+	let address_lat = address_geo.location.lat
+	let address_lng = address_geo.location.lng
+        
+	let pantries = await findPantries(db, address_lat, address_lng);
 	    
-	    address_res = addressLatLon.candidates[0]
-	    formatted_address = address.formatted_address
-	    
-	    address_geo = address_res.geometry
-	    address_lat = address_geo.location.lat
-	    address_lng = address_geo.location.lng
-           
-	    console.log("Querying sqlite database...");
-	    return pantriesPromise = findPantries(db, address_lat, address_lng);
-	});
-	    
-	dbResult.then((pantries) => {
-	    let row = pantries[0];
-	    respondWithPantry(formatted_address, address_lat, address_lng, row.name, row.address, row.city, row.state, callback);
-	});
+	let validPantriesPromises = pantries.map((pantry) => {return {"pantry": pantry, "valid": isValidPantryForCity(db, city, pantry.id)}});
+	let validPantries = validPantriesPromises.filter(async function(pantry) {return await pantry.valid})
+	console.log(validPantries)
+	let pantry = validPantries[0].pantry
+	
+	respondWithPantry(formatted_address, address_lat, address_lng, pantry.name, pantry.address, pantry.city, pantry.state, callback);
+	
 	
     });
     
